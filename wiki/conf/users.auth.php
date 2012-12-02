9e4130fbf7eccba8c7cf9451188da7ff698b558c
Micha Niskin <asdf@ubergibson.com>
2012-12-02 15:05:04 +0000

    scheduled commit

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -13,3 +13,4 @@ tdem:REDACTED:Thomas de Meijer:106498@gmail.com:admin,user
 admin:REDACTED:Administrator:106498@gmail.com:admin,user
 micha:REDACTED:Micha Niskin:micha.niskin@gmail.com:admin,user
 kevin_o_neill:REDACTED:Kevin O'Neill:K_S_ONeill@yahoo.com:admin,user
+alexander:REDACTED:alex:alex.niffeler@gmail.com:user

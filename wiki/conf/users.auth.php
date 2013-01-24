f6b7ce5bfeaa2d63e8c2ef42b2eda9863893b8ae
Micha Niskin <asdf@ubergibson.com>
2013-01-24 09:05:05 +0000

    scheduled commit

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -23,3 +23,4 @@ yoko731:REDACTED:Terrell Loveland:yW6Xg1aU@gmail.com:user
 carolina486:REDACTED:Cathy Larochelle:yW6Xg1aU@gmail.com:user
 renato157:REDACTED:Britta Vanwormer:euAq9V3v@gmail.com:user
 roxann853:REDACTED:Kimberlee Gillis:jagger.t@maxara-boat.com:user
+teddy524:REDACTED:Emory Kimsey:bakima@al-election2001.net:user

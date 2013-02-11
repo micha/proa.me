e670eb2a47013b4b143053b079a029aab1fc36ad
Micha Niskin <asdf@ubergibson.com>
2013-02-11 22:05:04 +0000

    scheduled commit

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -49,3 +49,4 @@ isiah53:REDACTED:Kellee Windham:s.u.sie.j2010.0.3@gmail.com:user
 abbiet84:REDACTED:Abbie Townsend:abbiet84@nablr.com:user
 britany679:REDACTED:Natashia Tamayo:modemperch98@hotmail.com:user
 digna36:REDACTED:Lia Bottom:beer79news@hotmail.com:user
+kirk982:REDACTED:Jeanna Limon:bath84trowel@hotmail.com:user

7f2a174de41d70b4bb5fb468b778fc7cd1d0a7be
Micha Niskin <asdf@ubergibson.com>
2013-03-16 14:05:06 +0000

    scheduled commit

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -883,3 +883,4 @@ desmond252:REDACTED:Dinah Baptiste:04RyLGb0@charlesmacklin.com:user
 danica889:REDACTED:Sophie Revell:SNaGufqR@gmail.com:user
 rosie546:REDACTED:Quentin Bragg:SNaGufqR@gmail.com:user
 enriqueta791:REDACTED:Bessie Milam:suit9gender@hotmail.com:user
+wilford383:REDACTED:Gabrielle Cummings:farheen1226@gmail.com:user

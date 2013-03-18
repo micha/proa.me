d493f854d39ae3479fd3a61aaf2d71e893199810
Micha Niskin <asdf@ubergibson.com>
2013-03-18 13:05:07 +0000

    scheduled commit

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -893,3 +893,4 @@ lois535:REDACTED:Mary Welty:wikirobot@zarabiajzfc.pl:user
 bryant199:REDACTED:Catharine Shroyer:email@facebook-spy.org:user
 tamra572:REDACTED:Hana Nolan:bb@premiumarea.eu:user
 catharine256:REDACTED:Rolland Dunkin:VotiysebcentaCrumpton@hotmail.com:user
+lovella72:REDACTED:Leta Everette:JohnsWikiWorld@gmail.com:user

2e7d9e314c81abd54836983fca95a9a265c282eb
Micha Niskin <asdf@ubergibson.com>
2013-04-01 09:05:33 +0000

    scheduled commit

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -1135,3 +1135,4 @@ enid463:REDACTED:Rafaela Geyer:toto99@beyaas.com:user
 enriqueta314:REDACTED:Ruby Haught:toto99@beyaas.com:user
 graig54:REDACTED:Arron Midkiff:toto99@beyaas.com:user
 charlie969:REDACTED:Mindy Hames:EYFDYc3N@gmail.com:user
+zachary611:REDACTED:Harold Latimer:leocopper15@hotmail.com:user

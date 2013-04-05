d81123fa651f20e69cf26ee0d361d574035b20b1
Micha Niskin <asdf@ubergibson.com>
2013-04-05 10:05:39 +0000

    scheduled commit

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -1205,3 +1205,4 @@ eula814:REDACTED:Hollis Bolyard:nSpwQLPE@gmail.com:user
 yoko687:REDACTED:Eddy Eberhart:shawnee915@blogsitebuilderreview.net:user
 archie67:REDACTED:Katy Dunne:traegt@the-best-juicer-reviews.com:user
 ardis191:REDACTED:Yessenia Spradling:nupatysicah@hotmail.com:user
+kris778:REDACTED:Arleen Stines:tQpoYBfa@gmail.com:user
